cbfe0e5d2ea78674a11e6f2b345f5b9e4d495b16
Kumar Ashwin <krashwin00@gmail.com>
2019-11-07 21:58:48 +0530

    added comment

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import sys
 from folder_organizer import os, operations, organize
 def main():
-    organize(sys.argv[1])
+    organize(sys.argv[1]) #takes the command line argument as input to script
 
 if __name__ == '__main__' : 
     main()
